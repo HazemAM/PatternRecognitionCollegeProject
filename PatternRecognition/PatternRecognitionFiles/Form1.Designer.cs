71dd8edf848eb613529f20a0a33f28a7e1c536d1
Hazem AbuMostafa <hbluebrince@gmail.com>
2014-10-31 00:03:49 +0200

    Initial release: base UI, open, generate.
    - The base UI.
    - Open common image formats.
    - Generate 4-sector images (without input).

---
+++ PatternRecognition/PatternRecognitionFiles/Form1.Designer.cs
@@ -0,0 +1,206 @@
+namespace PatternRecognition
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.grpboxTask1 = new System.Windows.Forms.GroupBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.leftPictureBox = new System.Windows.Forms.PictureBox();
+            this.rightPictureBox = new System.Windows.Forms.PictureBox();
+            this.saveDialog = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.grpboxTask1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnOpen.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Location = new System.Drawing.Point(391, 31);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(5);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(126, 62);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open Image";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // toolTip
+            // 
+            this.toolTip.AutomaticDelay = 200;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 86.54971F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 131F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(914, 561);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 138F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.btnOpen, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.grpboxTask1, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 433);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(908, 125);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // grpboxTask1
+            // 
+            this.grpboxTask1.Controls.Add(this.btnGenerate);
+            this.grpboxTask1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpboxTask1.Location = new System.Drawing.Point(3, 3);
+            this.grpboxTask1.Name = "grpboxTask1";
+            this.grpboxTask1.Size = new System.Drawing.Size(379, 119);
+            this.grpboxTask1.TabIndex = 2;
+            this.grpboxTask1.TabStop = false;
+            this.grpboxTask1.Text = "Task 1";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnGenerate.Location = new System.Drawing.Point(133, 64);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(100, 26);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate Image";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.leftPictureBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.rightPictureBox, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(914, 430);
+            this.tableLayoutPanel1.TabIndex = 13;
+            // 
+            // leftPictureBox
+            // 
+            this.leftPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.leftPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.leftPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.leftPictureBox.Margin = new System.Windows.Forms.Padding(0);
+            this.leftPictureBox.Name = "leftPictureBox";
+            this.leftPictureBox.Size = new System.Drawing.Size(457, 430);
+            this.leftPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.leftPictureBox.TabIndex = 0;
+            this.leftPictureBox.TabStop = false;
+            this.leftPictureBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox_DoubleClick);
+            this.leftPictureBox.MouseHover += new System.EventHandler(this.pictureBox_MouseHover);
+            // 
+            // rightPictureBox
+            // 
+            this.rightPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.rightPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rightPictureBox.Location = new System.Drawing.Point(457, 0);
+            this.rightPictureBox.Margin = new System.Windows.Forms.Padding(0);
+            this.rightPictureBox.Name = "rightPictureBox";
+            this.rightPictureBox.Size = new System.Drawing.Size(457, 430);
+            this.rightPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.rightPictureBox.TabIndex = 11;
+            this.rightPictureBox.TabStop = false;
+            this.rightPictureBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox_DoubleClick);
+            this.rightPictureBox.MouseHover += new System.EventHandler(this.pictureBox_MouseHover);
+            // 
+            // saveDialog
+            // 
+            this.saveDialog.DefaultExt = "jpg";
+            this.saveDialog.FileName = "image";
+            this.saveDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.saveDialog_FileOk);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 561);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pattern Recognition";
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.grpboxTask1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox leftPictureBox;
+        private System.Windows.Forms.PictureBox rightPictureBox;
+        private System.Windows.Forms.SaveFileDialog saveDialog;
+        private System.Windows.Forms.GroupBox grpboxTask1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ PatternRecognition/PatternRecognitionFiles/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PatternRecognition
+{
+    public partial class mainForm : Form
+    {
+        public mainForm(){
+            InitializeComponent();
+        }
+
+        Bitmap theBitmapImage;
+        Image  imageToSave=null;
+
+        private void btnOpen_Click(object sender, EventArgs e){
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if(fileDialog.ShowDialog()==DialogResult.OK){
+                //Open the image:
+                String imageFilePath = fileDialog.FileName.ToLower();
+                if(imageFilePath.EndsWith(".jpg") || imageFilePath.EndsWith(".png") ||
+                   imageFilePath.EndsWith(".gif") || imageFilePath.EndsWith(".bmp")){
+                    theBitmapImage = new Bitmap(imageFilePath);
+                }
+                else{
+                    MessageBox.Show("This type of files is not supported... yet.\n(Are you sure it's an image?)",
+                        "Not Supported", MessageBoxButtons.OK);
+                    return;
+                }
+                leftPictureBox.Image = theBitmapImage;
+            }
+        }
+
+        private void pictureBox_MouseHover(object sender, EventArgs e)
+        {
+            PictureBox pictureBox = ((PictureBox)sender);
+            if(pictureBox.Image != null){
+                toolTip.Active = true;
+                toolTip.SetToolTip(pictureBox, "Double click to save");
+            }
+            else
+                toolTip.Active = false;
+        }
+
+        private void pictureBox_DoubleClick(object sender, EventArgs e){
+            //Save the image.
+            if(((PictureBox)sender).Image!=null){
+                saveDialog.FileName = "image.jpg";
+                saveDialog.Filter   = "JPEG|*.jpg|All files|*.*";
+                imageToSave = ((PictureBox)sender).Image;
+                saveDialog.ShowDialog();
+            }
+        }
+
+        private void saveDialog_FileOk(object sender, CancelEventArgs e){
+            imageToSave.Save(saveDialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+            imageToSave = null;
+        }
+
+        private Bitmap generateImage(int width, int height)
+        {
+            Bitmap bitmap = new Bitmap(width, height);
+            int sectorWidth = width/4;
+            Color color1 = Color.FromArgb(10,150,255); //Color of sector #1,
+            Color color2 = Color.FromArgb(255,150,10); //...etc.
+            Color color3 = Color.FromArgb(10,255,150);
+            Color color4 = Color.FromArgb(150,10,255);
+            for(int y=0; y<height; y++) //Write the pixels
+                for(int x=0; x<sectorWidth; x++){
+                    bitmap.SetPixel(x, y, color1);
+                    bitmap.SetPixel(x+sectorWidth*1, y, color2);
+                    bitmap.SetPixel(x+sectorWidth*2, y, color3);
+                    bitmap.SetPixel(x+sectorWidth*3, y, color4);
+                }
+            return bitmap;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            leftPictureBox.Image = generateImage(350,350);
+        }
+    }
+}
