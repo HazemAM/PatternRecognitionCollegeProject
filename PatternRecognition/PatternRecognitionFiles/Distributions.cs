e93fcc5fc53490555671c96810897fd87f3cfa00
chaosagex <hk2nasr@gmail.com>
2014-11-05 01:19:19 +0200

    lil sth on dists

--- PatternRecognition/PatternRecognitionFiles/Distributions.cs
+++ PatternRecognition/PatternRecognitionFiles/Distributions.cs
@@ -8,7 +8,7 @@ namespace PatternRecognition
     {
         public static double Normal(double x, double mu, double sigma)
         {
-            return (1 / Math.Sqrt(2 * Math.PI) * sigma) * Math.Exp(-Math.Pow((x - mu), 2) / (2 * (Math.Pow(sigma,2))));
+            return (1 / (Math.Sqrt(2 * Math.PI) * sigma)) * Math.Exp(-Math.Pow((x - mu), 2) / (2 * (Math.Pow(sigma,2))));
         }
     }
 }
